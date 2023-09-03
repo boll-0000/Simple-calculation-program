5de21c872c7d8d6ba735c13299142e396bae4003
SBCORLALT-C0H000 <133245361+boll-0000@users.noreply.github.com>
2023-09-04 00:20:09 +0900

    Update program.py

--- program.py
+++ program.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
-language = int(input('Enter 0 for Japanese or 1 for English.　'))#言語選択
 
-if language == 0:#日本語
-  print('答えは',int(input('足される数字は？　'))+int(input('足す数字は？　')),'です')#日本語で計算結果を表示
-elif language == 1:#英語
-  print('The answer is',int(input('What is the number that is added?　'))+int(input('What is the number to add?　')),'.')#View calculation results in English
-else:#その他
-  print('その言語はサポートされていません')#エラー(Error)
-#プログラムの終了
+# 言語を選択
+language = int(input('日本語を選ぶ場合は0、英語を選ぶ場合は1を入力してください。 '))
+
+if language == 0:
+    # 日本語で計算結果を表示
+    print('答えは', int(input('足される数字は？ ')) + int(input('足す数字は？ ')), 'です')
+elif language == 1:
+    # 英語で計算結果を表示
+    print('The answer is', int(input('What is the number that is added? ')) + int(input('What is the number to add? ')), '.')
+else:
+    # サポートされていない言語のエラーメッセージ
+    print('この言語はサポートされていません')
